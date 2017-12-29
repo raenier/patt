0f6d94e8901a201a0ded306f01ca883b0fa4a3a9
raenier <raenierquiambao@gmail.com>
2017-12-29 09:51:58 +0800

    Remove documentation for common functions

--- lib/patt/attendance/attendance.ex
+++ lib/patt/attendance/attendance.ex
@@ -8,109 +8,32 @@ defmodule Patt.Attendance do
 
   alias Patt.Attendance.Employee
 
-  @doc """
-  Returns the list of employees.
-
-  ## Examples
-
-      iex> list_employees()
-      [%Employee{}, ...]
-
-  """
   def list_employees do
     Repo.all(Employee)
   end
 
-  @doc """
-  Gets a single employee.
-
-  Raises `Ecto.NoResultsError` if the Employee does not exist.
-
-  ## Examples
-
-      iex> get_employee!(123)
-      %Employee{}
-
-      iex> get_employee!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_employee!(id), do: Repo.get!(Employee, id)
 
-  @doc """
-  Creates a employee.
-
-  ## Examples
-
-      iex> create_employee(%{field: value})
-      {:ok, %Employee{}}
-
-      iex> create_employee(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_employee(attrs \\ %{}) do
     %Employee{}
     |> Employee.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a employee.
-
-  ## Examples
-
-      iex> update_employee(employee, %{field: new_value})
-      {:ok, %Employee{}}
-
-      iex> update_employee(employee, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_employee(%Employee{} = employee, attrs) do
     employee
     |> Employee.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Employee.
-
-  ## Examples
-
-      iex> delete_employee(employee)
-      {:ok, %Employee{}}
-
-      iex> delete_employee(employee)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_employee(%Employee{} = employee) do
     Repo.delete(employee)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking employee changes.
-
-  ## Examples
-
-      iex> change_employee(employee)
-      %Ecto.Changeset{source: %Employee{}}
-
-  """
   def change_employee(%Employee{} = employee) do
     Employee.changeset(employee, %{})
   end
 
-
-  @doc """
-  Search for either employee_id or employee_name
-  ## Requires a string argument
-
-  ## Returns
-  [%Patt.Attendance.Employee{}]
-
-  """
   def search_employee(params) do
     case Integer.parse(params) do
       {number, _} ->
