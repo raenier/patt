ea948ecefc9f52da2296df7cba2ae3377fdbf84e
raenier <raenierquiambao@gmail.com>
2017-12-29 13:40:34 +0800

    Remove null constraint on creating Employee
    remove null constraint for position on employee
    can create employee without the position

--- priv/repo/migrations/20171229041556_add_a_reference_to_employee_from_position.exs
+++ priv/repo/migrations/20171229041556_add_a_reference_to_employee_from_position.exs
@@ -3,7 +3,7 @@ defmodule Patt.Repo.Migrations.AddAReferenceToEmployeeFromPosition do
 
   def change do
     alter table(:employees) do
-      add :position_id, references(:positions, on_delete: :nothing), null: false
+      add :position_id, references(:positions, on_delete: :nothing)
     end
 
     create index(:employees, [:position_id])
