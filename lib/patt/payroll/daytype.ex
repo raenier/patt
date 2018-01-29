4444d85a9cde54b8f53196b97eae8c7ac85f3b40
raenier <raenierquiambao@gmail.com>
2018-01-29 22:03:24 +0800

    remove daytype schema just use a normal struct

--- lib/patt/payroll/daytype.ex
+++ lib/patt/payroll/daytype.ex
@@ -1,20 +1,2 @@
 defmodule Patt.Payroll.Daytype do
-  use Ecto.Schema
-  import Ecto.Changeset
-  alias Patt.Payroll.Daytype
-
-
-  schema "daytypes" do
-    field :rate, :integer
-    field :type, :string
-
-    timestamps()
-  end
-
-  @doc false
-  def changeset(%Daytype{} = daytype, attrs) do
-    daytype
-    |> cast(attrs, [:type, :rate])
-    |> validate_required([:type, :rate])
-  end
 end
