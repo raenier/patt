835a8f75bc310514a87ee618356e0b4a8079ed39
raenier <raenierquiambao@gmail.com>
2018-02-05 10:21:52 +0800

    Add day of week on display of date

--- lib/patt_web/views/payroll_view.ex
+++ lib/patt_web/views/payroll_view.ex
@@ -14,6 +14,10 @@ defmodule PattWeb.PayrollView do
     put_zero(start) <> ":" <> put_zero(endt)
   end
 
+  def return_date_and_day(date) do
+    "#{date}" <> " " <> Patt.Helper.return_day_string(Date.day_of_week(date))
+  end
+
   def assoc_loaded?(data) do
     Ecto.assoc_loaded? data
   end
