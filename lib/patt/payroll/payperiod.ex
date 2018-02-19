b89d6bdc27515b247e15ce19d8cf47778de4ff54
raenier <raenierquiambao@gmail.com>
2018-02-19 13:40:31 +0800

    Create Payroll.Payperiod schema

---
+++ lib/patt/payroll/payperiod.ex
@@ -0,0 +1,20 @@
+defmodule Patt.Payroll.Payperiod do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Patt.Payroll.Payperiod
+
+
+  schema "payperiods" do
+    field :from, :date
+    field :to, :date
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Payperiod{} = payperiod, attrs) do
+    payperiod
+    |> cast(attrs, [:from, :to])
+    |> validate_required([:from, :to])
+  end
+end
