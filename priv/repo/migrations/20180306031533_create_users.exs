13bdce8127d26743d254f5df90b6556a2f4e05b8
raenier <raenierquiambao@gmail.com>
2018-03-06 14:21:05 +0800

    Require username and password_hash to be non empty

--- priv/repo/migrations/20180306031533_create_users.exs
+++ priv/repo/migrations/20180306031533_create_users.exs
@@ -3,12 +3,14 @@ defmodule Patt.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :username, :string
-      add :password_hash, :string
+      add :username, :string, null: false
+      add :password_hash, :string, null: false
 
       timestamps()
     end
 
     create unique_index(:users, [:username])
+    create constraint(:users, :username_is_required, check: "char_length(username) > 0")
+    create constraint(:users, :password_hash_is_required, check: "char_length(password_hash) > 0")
   end
 end
